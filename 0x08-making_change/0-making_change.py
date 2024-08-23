14e5f5bf04e70a409364193518ab4089934c759c
Antonymaged <magedantony1@gmail.com>
2024-08-23 01:51:00 +0000

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,15 +6,19 @@
 def makeChange(coins, total):
 
     """Returns: fewest number of coins needed to get to the total"""
-     dp = [float('inf')] * (total + 1)
-
-    # Base case: 0 coins are needed to make a total of 0
-    dp[0] = 0
-
-    # Fill the DP array
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    # If dp[total] is still infinity, return -1 (total cannot be met)
-    return dp[total] if dp[total] != float('inf') else -1
+     if(total <= 0):
+        return 0
+    coins = [x for x in coins if x <= total]
+    maxi = max(coins)
+    sum = 0
+    ans = 0
+    while sum <= total:
+        if(sum == total):
+            return ans
+        while(sum + maxi > total):
+            coins.remove(maxi)
+            if coins == []:
+                return -1
+            maxi = max(coins)
+        sum += maxi
+        ans += 1
